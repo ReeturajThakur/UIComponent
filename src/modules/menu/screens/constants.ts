d8095de3515d29a6ee2974860d8ef6137fe0cf3f
Kdsinghak <kuldeep.singh1@appinventiv.com>
2023-02-27 16:45:48 +0530

    localImages

--- src/modules/menu/screens/constants.ts
+++ src/modules/menu/screens/constants.ts
@@ -1,3 +1,5 @@
+import localImages from '../../../utils/localImages';
+
 export const menuData = [
   {
     rightIcon: '',
@@ -7,8 +9,70 @@ export const menuData = [
   {heading: 'Key ShortCuts'},
 
   {
-    rightIcon: '',
-    label: 'Seva',
+    rightIcon: localImages.sewa,
+    label: 'Sewa',
+    leftIcon: '',
+  },
+  {
+    rightIcon: localImages.tasks,
+    label: 'Tasks',
+    leftIcon: '',
+  },
+  {
+    rightIcon: localImages.events,
+    label: 'Events',
+    leftIcon: '',
+  },
+  {
+    rightIcon: localImages.mettings,
+    label: 'Meetings',
+    leftIcon: '',
+  },
+  {
+    rightIcon: localImages.Community_Hall,
+    label: 'Community Hall',
+    leftIcon: '',
+  },
+  {
+    rightIcon: localImages.saved_post,
+    label: 'Saved Posts',
+    leftIcon: '',
+  },
+  {
+    rightIcon: localImages.feeds,
+    label: 'Feeds',
+    leftIcon: '',
+  },
+  {
+    rightIcon: localImages.stories,
+    label: 'Stories',
+    leftIcon: '',
+  },
+  {heading: 'Settings'},
+  {
+    rightIcon: localImages.privacySetting,
+    label: 'Privacy Settings',
+    leftIcon: '',
+  },
+  {
+    rightIcon: localImages.accountSetting,
+    label: 'Account settings',
+    leftIcon: '',
+  },
+  {
+    rightIcon: localImages.notificationSetting,
+    label: 'Notification settings',
+    leftIcon: '',
+  },
+  {
+    rightIcon: localImages.notificationSetting,
+    label: 'Language settings',
+    leftIcon: '',
+  },
+  {heading: 'Contact'},
+  {
+    rightIcon: localImages.helpsupport,
+    label: 'Help & support',
     leftIcon: '',
   },
 ];

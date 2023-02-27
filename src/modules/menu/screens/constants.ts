6a6bc37c833256e978bde14ea3bc1610e2112f07
Kdsinghak <kuldeep.singh1@appinventiv.com>
2023-02-27 16:26:44 +0530

    images add

---
+++ src/modules/menu/screens/constants.ts
@@ -0,0 +1,14 @@
+export const menuData = [
+  {
+    rightIcon: '',
+    label: 'About community',
+    leftIcon: '',
+  },
+  {heading: 'Key ShortCuts'},
+
+  {
+    rightIcon: '',
+    label: 'Seva',
+    leftIcon: '',
+  },
+];
